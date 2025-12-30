21b4d970b1fdec4b5944f1cfd78d764bad244697
Chris <richter1905@web.de>
2025-12-30 20:18:34 +0100

    - write unit tests for PaginationUtils class - add private constructor to PaginationUtils class that throws exception when tried to be instantiated.

--- apps/backend/src/main/java/com/flightlogger/backend/common/utils/PaginationUtils.java
+++ apps/backend/src/main/java/com/flightlogger/backend/common/utils/PaginationUtils.java
@@ -5,6 +5,10 @@ import org.springframework.data.domain.Page;
 
 public class PaginationUtils {
 
+    private PaginationUtils() {
+        throw new UnsupportedOperationException("PaginationUtils is a utility class and cannot be instantiated");
+    }
+
     public static PaginationMetadata toMetaData(Page<?> page) {
         return new PaginationMetadata(page.getTotalElements(), page.getTotalPages(), page.getNumber(), page.getSize());
     }
